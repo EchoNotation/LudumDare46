32b0452e1fa8d9c04e9536d8cf799c9a74050576
Thomas <thetardis1968@gmail.com>
2020-04-18 15:43:40 -0500

    Made the assassin and king also be at z=-1

--- ForTheKing/Assets/Scripts/Assassin.cs
+++ ForTheKing/Assets/Scripts/Assassin.cs
@@ -19,7 +19,7 @@ public class Assassin : MonoBehaviour
     {
         Tilemap tiles = FindObjectOfType<Tilemap>();
         int gridSize = tiles.size.y;
-        Vector3 offset = new Vector3(tiles.cellSize.x / 2, tiles.cellSize.x / 2, 0);
+        Vector3 offset = new Vector3(tiles.cellSize.x / 2, tiles.cellSize.x / 2, -1);
         transform.position = tiles.CellToWorld(new Vector3Int(gridX - gridSize / 2, gridY - gridSize / 2, -1)) + offset;
     }
 
