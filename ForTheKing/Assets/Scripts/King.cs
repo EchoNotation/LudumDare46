c52fc357ae4f7cb48e840cf2a3ef2cfe468ee081
Thomas <thetardis1968@gmail.com>
2020-04-18 13:17:28 -0500

    Preliminary raycasting/physics

---
+++ ForTheKing/Assets/Scripts/King.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class King : MonoBehaviour
+{
+    public int gridX = 0;
+    public int gridY = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilemap tiles = FindObjectOfType<Tilemap>();
+        int gridSize = tiles.size.y;
+        Vector3 offset = new Vector3(tiles.cellSize.x / 2, tiles.cellSize.x / 2, 0);
+        transform.position = tiles.CellToWorld(new Vector3Int(gridX - gridSize / 2, gridY - gridSize / 2, 0)) + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
